ceeeb40a618c9f5e08d84fde3d5e856f01711131
swiftybathero <swiftybathero@gmail.com>
2017-04-12 21:49:44 +0200

    Window content implementation, minor command changes
    Changed WindowFrame buttons commands
    Added ContentControl to WindowFrame Content
    Added LoginView and LoginViewModel
    Added DataTemplate for LoginViewModel
    Added ViewModelBase class for INotifyPropertyChanged implementation
    Added CurrentViewModel property for WindowFrame content management

---
+++ ClansApp.UI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClansApp.UI.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public string Text { get; set; }
+
+        public LoginViewModel()
+        {
+            Text = "SHOWCASE TEXT";
+        }
+    }
+}
--- ClansApp.UI/ViewModels/WindowFrameViewModel.cs
+++ ClansApp.UI/ViewModels/WindowFrameViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ClansApp.UI.Extensions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,29 @@ using System.Windows.Input;
 
 namespace ClansApp.UI.ViewModels
 {
-    public class WindowFrameViewModel
+    public class WindowFrameViewModel : ViewModelBase
     {
+        public ViewModelBase CurrentViewModel { get; set; }
+
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get
+            {
+                return _windowState;
+            }
+            set
+            {
+                _windowState = value;
+                OnPropertyChanged(nameof(WindowState));
+            }
+        }
+
         public ICommand CloseWindowCommand { get; set; }
         public ICommand MinimizeWindowCommand { get; set; }
+        /// <summary>
+        /// Currently unused - was used by Interaction.Trigger from System.Windows.Interactivity
+        /// </summary>
         public ICommand MoveWindowCommand { get; set; }
         public ICommand MaximizeWindowCommand { get; set; }
         public ICommand RestoreWindowCommand { get; set; }
@@ -20,10 +40,12 @@ namespace ClansApp.UI.ViewModels
         public WindowFrameViewModel()
         {
             CloseWindowCommand = new RelayCommand<object>((o) => (o as Window).Close() /*App.Current.Shutdown()*/);
-            MoveWindowCommand = new RelayCommand<object>((o) => (o as Window).DragMove());
-            MinimizeWindowCommand = new RelayCommand<object>((o) => (o as Window).WindowState = WindowState.Minimized);
-            MaximizeWindowCommand = new RelayCommand<object>((o) => (o as Window).WindowState = WindowState.Maximized);
-            RestoreWindowCommand = new RelayCommand<object>((o) => (o as Window).WindowState = WindowState.Normal);
+            //MoveWindowCommand = new RelayCommand<object>((o) => (o as Window).DragMove());
+            MinimizeWindowCommand = new RelayCommand<object>((o) => WindowState = WindowState.Minimized);
+            MaximizeWindowCommand = new RelayCommand<object>((o) => WindowState = WindowState.Maximized);
+            RestoreWindowCommand = new RelayCommand<object>((o) => WindowState = WindowState.Normal);
+
+            CurrentViewModel = new LoginViewModel();
         }
     }
 }
