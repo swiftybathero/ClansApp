f1e67dc89e1e1849501f3c34d9defd457ab97850
swiftybathero <swiftybathero@gmail.com>
2017-05-06 16:38:57 +0200

    Added NUnit test project with basic tests

---
+++ ClansApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClansApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClansApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49395cff-c00e-4da7-9df9-10efb6009728")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClansApp.Tests/WrapperTests.cs
@@ -0,0 +1,45 @@
+﻿using ClansApp.Data.Models;
+using ClansApp.UI.Wrappers;
+using NUnit.Framework;
+using System;
+
+namespace ClansApp.Tests
+{
+    [TestFixture]
+    public class WrapperTests
+    {
+        private Member member;
+        private MemberWrapper memberWrapper;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            member = new Member()
+            {
+                Name = "Test member",
+                Donations = 7231
+            };
+            memberWrapper = new MemberWrapper(member);
+        }
+
+        [Test]
+        public void IsMemberEqualToWrapper()
+        {
+            foreach (var property in member.GetType().GetProperties())
+            {
+                var wrapperProperty = memberWrapper.GetType().GetProperty(property.Name);
+                if (wrapperProperty != null)
+                {
+                    Assert.AreEqual(property.GetValue(member), wrapperProperty.GetValue(memberWrapper));
+                }
+            }
+        }
+
+        [Test]
+        public void DoesWrapperChangeAffectsMember()
+        {
+            memberWrapper.Role = "Leader";
+            Assert.AreEqual(member.Role, memberWrapper.Role);
+        }
+    }
+}
