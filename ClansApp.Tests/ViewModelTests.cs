fc8347b7bd58f250a8aa6cca74960e2155522cfc
swiftybathero <swiftybathero@gmail.com>
2017-06-06 22:28:44 +0200

    Additional unit tests

--- ClansApp.Tests/ViewModelTests.cs
+++ ClansApp.Tests/ViewModelTests.cs
@@ -1,4 +1,6 @@
 ﻿using ClansApp.Data.Models;
+using ClansApp.UI.Extensions;
+using ClansApp.UI.Serializers;
 using ClansApp.UI.Services;
 using ClansApp.UI.Services.Messages;
 using ClansApp.UI.ViewModels;
@@ -17,12 +19,21 @@ namespace ClansApp.Tests
     [TestFixture]
     public class ViewModelTests
     {
+        #region Objects/collections
         private WindowFrameViewModel _windowFrameViewModel;
-        private ObservableCollection<MemberWrapper> _memberWrapperCollection;
+        private ObservableCollection<MemberWrapper> _memberWrapperCollection; 
+        #endregion
+
+        #region Mocks
+        private Mock<IClansDataService> _clansDataServiceMock;
+        private Mock<WindowFrameViewModel> _windowFrameViewModelMock;
+        private Mock<ISettingsSerializer> _settingsSerializerMock;
+        #endregion
 
         [SetUp]
         public void SetUp()
         {
+            // fill the collection
             _memberWrapperCollection = new ObservableCollection<MemberWrapper>
             {
                 new MemberWrapper(new Member() {Name = "First member name", Role = "Test role"}),
@@ -30,13 +41,33 @@ namespace ClansApp.Tests
                 new MemberWrapper(new Member() {Name = "Third member name", Role = "Test role"})
             };
 
-            var clansDataServiceMock = new Mock<IClansDataService>();
-            clansDataServiceMock.Setup(x => x.GetAllMembersAsync()).ReturnsAsync(_memberWrapperCollection);
+            // data service mock
+            _clansDataServiceMock = new Mock<IClansDataService>();
+            _clansDataServiceMock.Setup(x => x.GetAllMembersAsync()).ReturnsAsync(_memberWrapperCollection);
 
-            _windowFrameViewModel = new WindowFrameViewModel(clansDataServiceMock.Object, null);
+            // settings serializer mock
+            _settingsSerializerMock = new Mock<ISettingsSerializer>();
+
+            // pass mock object to constructor
+            _windowFrameViewModelMock = new Mock<WindowFrameViewModel>(_clansDataServiceMock.Object, _settingsSerializerMock.Object);
+            _windowFrameViewModel = _windowFrameViewModelMock.Object;
 
             // to call ShowMembersData private method
             Messenger.Default.Send(new LoginMessage(null, string.Empty));
+        }
+
+        [Test]
+        public void WasGetAllMembersAsyncCalledOnce()
+        {
+            // check if method was called only once
+            _clansDataServiceMock.Verify(v => v.GetAllMembersAsync(), Times.Once);
+        }
+
+        [Test]
+        public void AreSettingsSavedOnLogin()
+        {
+            _windowFrameViewModel.LoginViewModel.LoginCommand.Execute(null);
+            _settingsSerializerMock.Verify(v => v.SaveAPIKey(It.IsAny<string>()), Times.AtLeastOnce);
         }
 
         [Test]
